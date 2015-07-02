a2ef847a13f44af8e42838fd29250822268299b6
Tim Ashton <t.j.ashton1@gmail.com>
2015-07-02 21:06:00 +1000

    #4: Dim the screen when the app launches

--- Bright-Flashlight/bright-flashlight/src/main/java/com/timashton/bright_flashlight/MainActivity.java
+++ Bright-Flashlight/bright-flashlight/src/main/java/com/timashton/bright_flashlight/MainActivity.java
@@ -3,26 +3,52 @@ package com.timashton.bright_flashlight;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
 
+    public static final String TAG = MainActivity.class.getName();
     public static final String PREFS_SHOW_DIALOG = "show_dialog_boolean";
 
     @SuppressWarnings("deprecation")
     private Camera mCamera;
+    private int mSavedBrightness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Dim the screen. The user is probably in a dark place
+        WindowManager.LayoutParams params = this.getWindow().getAttributes();
+        params.screenBrightness = 0.0f;
+        this.getWindow().setAttributes(params);
+
+        // Get the camera and start the LED light
         if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
 
             mCamera = Camera.open();
+            try{
+                // need to set a SurfaceTexture for nexus and maybe other new devices
+                mCamera.setPreviewTexture(new SurfaceTexture(0));
+            }
+            catch (IOException ioe){
+                Log.e(TAG, ioe.toString());
+            }
+
             Camera.Parameters p = mCamera.getParameters();
             p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
             mCamera.setParameters(p);
@@ -35,13 +61,13 @@ public class MainActivity extends Activity {
         // Restore preferences
         SharedPreferences settings = getPreferences(MainActivity.MODE_PRIVATE);
         boolean showDialog = settings.getBoolean(PREFS_SHOW_DIALOG, true);
-        if(showDialog){
+        if (showDialog) {
             RatingDialogFragment ratingDialogFragment = RatingDialogFragment.newInstance();
             ratingDialogFragment.show(
                     getFragmentManager().beginTransaction(), RatingDialogFragment.TAG);
         }
-    }
 
+    }
 
     @Override
     protected void onPause() {
@@ -53,5 +79,4 @@ public class MainActivity extends Activity {
         // Finish the activity
         finish();
     }
-
 }
